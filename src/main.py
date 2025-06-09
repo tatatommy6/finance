3a9b93a7e802f544856247fad028bfb95ac8cf58
김민결 <139607260+tatatommy6@users.noreply.github.com>
2025-06-10 01:55:13 +0900

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import yfinance as yf
+
+stock_ticker = ['AAPL','NVDA','GOOG']
+start_ = '2022-06-09'
+end_ = '2025-06-09'
+for ticker in stock_ticker:
+    df = yf.download(ticker, start=start_, end=end_ )
+
+    #csv 
+    df.to_csv(f'data/{ticker}_data.csv', index=True)
+    print(f"{ticker}주가 저장완료")
