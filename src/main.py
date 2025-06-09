ae5dc07f127dab409959d3031ba4f3296643c064
김민결 <139607260+tatatommy6@users.noreply.github.com>
2025-06-10 02:11:39 +0900

    오늘은 끝
    진짜 쓸데없는 커밋..

--- src/main.py
+++ src/main.py
@@ -7,6 +7,6 @@ end_ = '2025-06-09'
 for ticker in stock_ticker:
     df = yf.download(ticker, start=start_, end=end_ )
 
-    #csv 
+    #csv로 저장
     df.to_csv(f'data/{ticker}_data.csv', index=True)
-    print(f"{ticker}주가 저장완료")
+    print(f"{ticker}주가 저장완료") 
