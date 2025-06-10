4e63734c55fd53aa91a153ea769e5632b1bee36d
김민결 <139607260+tatatommy6@users.noreply.github.com>
2025-06-10 12:40:53 +0900

    애플만 일단 예측 해보자

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import yfinance as yf
+import pandas_datareader as pdr
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader, Dataset
 
-stock_ticker = ['AAPL','NVDA','GOOG']
+
+stock_ticker = ['AAPL']
 start_ = '2022-06-09'
 end_ = '2025-06-09'
 for ticker in stock_ticker:
@@ -10,3 +18,7 @@ for ticker in stock_ticker:
     #csv로 저장
     df.to_csv(f'data/{ticker}_data.csv', index=True)
     print(f"{ticker}주가 저장완료") 
+
+df['Close'].plot(kind='line', figsize=(8,4),title='appl Stock close ')
+plt.show()
+plt.savefig('AAPL_stock_close.png')
---
+++ src/train.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import yfinance as yf
+import pandas_datareader as pdr
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader, Dataset
+
